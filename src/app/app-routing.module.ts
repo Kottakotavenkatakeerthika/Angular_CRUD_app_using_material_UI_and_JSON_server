89135ef0464e834bf24899d07ca3402469f5c142
Rajesh Chowdary <paidirajesh2001@gmail.com>
2023-09-03 10:11:48 +0530

    Angular_CRUD_app_using_material_UI_and_JSON_server changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { DetailsViewComponent } from './details-view/details-view.component'; // Import your details view component
+import { DetailsViewComponent } from './details-view/details-view.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'app-details-view', component: DetailsViewComponent },
-  // Add other routes as needed
-  { path: '', redirectTo: '/login', pathMatch: 'full' } // Redirect to the login page by default
+  { path: '', redirectTo: '/login', pathMatch: 'full' } 
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
